b1bbc278fcf1483350b5b79b5b08e4e456241b2c
fushang318 <fushang318@gmail.com>
2016-07-21 18:18:51 +0800

    增加业务类别下的课件查询

--- app/decorators/bank/business_category_wares_decorator.rb
+++ app/decorators/bank/business_category_wares_decorator.rb
@@ -2,6 +2,18 @@ module Bank
   class BusinessCategory
     include MongoidSimpleRedisCache::BaseMethods
 
+    def simple_video_wares_db(post)
+      wares_db(KcCourses::SimpleVideoWare)
+    end
+
+    def simple_document_wares_db(post)
+      wares_db(KcCourses::SimpleDocumentWare)
+    end
+
+    def finance_teller_wares_db(post)
+      wares_db(Finance::TellerWare)
+    end
+
     def simple_video_wares_of_post_db(post)
       wares_of_post_db(post, KcCourses::SimpleVideoWare)
     end
@@ -24,6 +36,18 @@ module Bank
 
       bcs.map do |bc|
         bc.wares_of_post_db(post, ware_type_class)
+      end.inject do |wares1, wares2|
+        wares1 = wares1 & wares2
+      end
+    end
+
+    def wares_db(ware_type_class)
+      if self.leaf?
+        return ware_type_class.where(:business_category_ids.in => [self.id.to_s]).to_a
+      end
+
+      self.children.map do |bc|
+        bc.wares_db(ware_type_class)
       end.inject do |wares1, wares2|
         wares1 = wares1 & wares2
       end
