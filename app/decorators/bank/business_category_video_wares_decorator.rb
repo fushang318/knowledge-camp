87e73141fc7133c03fbb12098ae7585cc056ba61
fushang318 <fushang318@gmail.com>
2016-07-21 17:53:54 +0800

    增加文档课件和流程课件的查询

--- app/decorators/bank/business_category_video_wares_decorator.rb
+++ app/decorators/bank/business_category_video_wares_decorator.rb
@@ -3,14 +3,27 @@ module Bank
     include MongoidSimpleRedisCache::BaseMethods
 
     def simple_video_wares_of_post_db(post)
+      wares_of_post_db(post, KcCourses::SimpleVideoWare)
+    end
+
+    def simple_document_wares_of_post_db(post)
+      wares_of_post_db(post, KcCourses::SimpleDocumentWare)
+    end
+
+    def finance_teller_wares_of_post_db(post)
+      wares_of_post_db(post, Finance::TellerWare)
+    end
+
+    def wares_of_post_db(post, ware_type_class)
       if self.leaf?
-        return KcCourses::SimpleVideoWare.where(:business_category_ids.in => [self.id.to_s]).to_a
+        return ware_type_class.where(:business_category_ids.in => [self.id.to_s]).to_a
       end
 
       bcs = post.children_business_categories(self)
       return [] if bcs.blank?
+
       bcs.map do |bc|
-        bc.simple_video_wares_of_post_db(post)
+        bc.wares_of_post_db(post, ware_type_class)
       end.inject do |wares1, wares2|
         wares1 = wares1 & wares2
       end
