ee62cebb45a59b30af9872866547f589b0fe2c6d
destinyd <destinyd_war@163.com>
2016-07-27 12:04:08 +0800

    判断课件是否在柜员职位对应的业务分类里面

--- app/decorators/kc_courses/ware_decorator.rb
+++ app/decorators/kc_courses/ware_decorator.rb
@@ -3,4 +3,10 @@ KcCourses::Ware.class_eval do
   has_many :notes,     class_name: "NoteMod::Note",         as: :targetable
 
   has_and_belongs_to_many :business_categories, class_name: "Bank::BusinessCategory", inverse_of: nil
+
+  def in_business_categories?(user)
+    raise "用户不能为空" if user.nil?
+    categories = user.post.try(:business_categories).to_a
+    (categories & business_categories.to_a).any?
+  end
 end
